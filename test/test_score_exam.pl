22244770e2ab89b87d81da8a9742d4597492c89c
iuwu <dimitri.muralt@students.fhnw.ch>
2018-08-30 20:15:52 +0200

    test folder added, use module has to be researched

---
+++ test/test_score_exam.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+
+use strict;
+use warnings;
+use Test;
+# use a BEGIN block so we print our plan before MyModule is loaded
+BEGIN { plan tests => 14, todo => [3,4] }
+# load your module...
+#use ???;
