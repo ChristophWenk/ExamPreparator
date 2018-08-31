525fa8a4edd433fa8675565e06178a3f79223698
iuwu <dimitri.muralt@students.fhnw.ch>
2018-08-31 19:02:38 +0200

    added comments and data structure description

--- src/score_exams.pl
+++ src/score_exams.pl
@@ -12,9 +12,10 @@ use lib dirname(dirname abs_path $0) . '';
 use src::statistics qw(createStatistics);
 use Text::Levenshtein qw(distance);
 #usage
-#perl src/score_exams.pl FHNW_entrance_exam_master_file_2017.txt resources/SampleResponses/*
-#perl src/score_exams.pl FHNW_entrance_exam_master_file_2017.txt resources/SampleResponses/20170828-092520-FHNW_entrance_exam-ID006431
+#perl src/score_exams.pl resources/MasterFiles/FHNW_entrance_exam_master_file_2017.txt resources/SampleResponses/*
+#perl src/score_exams.pl resources/MasterFiles/FHNW_entrance_exam_master_file_2017.txt resources/SampleResponses/20170828-092520-FHNW_entrance_exam-ID006431
 
+# check console input
 if (@ARGV < 2) {
     say "Missing parameters. Usage:";
     say "$0 <master-file> [response-files]";
@@ -24,36 +25,44 @@ if (@ARGV < 2) {
 my @args = ($^O eq 'MSWin32') ? map { glob } @ARGV : @ARGV;
 my ($master_filename, @student_filenames) = @args;
 
-#====================================================================
-# Master Data
-#====================================================================
-my $master_file_path = "resources/MasterFiles/";
-$master_filename  = $master_file_path . $master_filename;
+my (%master_questions, %student_questions);  # structure:
+#                                               { question1 => {
+#                                                      option1 => selected,
+#                                                      option2 => selected,
+#                                                      option3 => selected,
+#                                                      option4 => selected,
+#                                                      option5 => selected },
+#                                                 question2 => {
+#                                                      option1 => selected,
+#                                                      option2 => selected,
+#                                                      option3 => selected,
+#                                                      option4 => selected,
+#                                                      option5 => selected }
+#
+#                                                 }
+my (%master_answers, %student_answers);     # structure:   { question1 => answer1, question2 => answer2 }
+my @student_score;                #structure: [count_correct, count_answered]
+my %students_scores;              #structure: {student1 => [count_correct, count_answered],
+#                                              student2 => [count_correct, count_answered] }
+my %students_answers;             #structure: {student1 => { question1 => answer1, question2 => answer2, .. },
+#                                              student2 => { question1 => answer1, question2 => answer2, .. }
+#                                             }
 
-my %master_questions = get_questions_with_options($master_filename);
-my %master_answers = get_answers(%master_questions);
-#====================================================================
-# Student Data
-#====================================================================
-my %students_scores;
-my %students_answers;
+# get master data
+%master_questions = get_data_from_file($master_filename);
+%master_answers   = get_answers(%master_questions);
 
+# get data for each student
 for my $student_filename (@student_filenames){
-    my %student_questions = get_questions_with_options($student_filename);
-    %student_questions = sanitize_questions($student_filename, %student_questions);
+    %student_questions = get_data_from_file($student_filename);
+    %student_questions = sanitize_student_data($student_filename, %student_questions);
+    %student_answers   = get_answers(%student_questions);
+    @student_score     = check_answers(%student_answers);
 
-    my %student_answers = get_answers(%student_questions);
-
-    #############################
-    # miscounduct
-    # Collect student answers
-    #############################
+    # collect student score for statistics
+    $students_scores{$student_filename} = [ @student_score ];
+    # collect student answers for misconduct check
     $students_answers{$student_filename} = {%student_answers};
-    ##############################
-    #Statistics
-    #collect student score
-    ##############################
-    $students_scores{$student_filename} = [ check_answers(%student_answers) ];
 }
 
 #print student score
@@ -68,33 +77,35 @@ createStatistics(%students_scores);
 # Subroutines
 #====================================================================
 
-sub get_questions_with_options($filename) {
+sub get_data_from_file($filename) {
 
     open(my $filehandle, "<", $filename) or die "Could not open file '$filename' $!" ;
 
     my %questions;
     my $current_question;
     my %current_options;
-
+    #process file row by row
     while (my $row = readline($filehandle)) {
         #####################################
         #todo: remove trim and adjust if regex
         #####################################
         $row =~ s/^\s+|\s+$//g;           # trim
-
-        if(substr($row,0,1) =~ /^\d/) {  # if row starts with a number
+        # if row starts with a number > its a question
+        if(substr($row,0,1) =~ /^\d/) {
             $current_question = $row;
         }
+        #if row starts with a "_" try to save a question with all options
         elsif((substr($row,0,1) eq '_' || substr($row,0,1) eq '=')
                 &&
-                defined($current_question)){ # save question with options
+                defined($current_question)){
             $questions{$current_question} = { %current_options };
             %current_options = ();
             $current_question = undef;
         }
-        elsif(substr($row,0,1) eq '[' && defined($current_question)) { #add option
+        #add option to current question
+        elsif(substr($row,0,1) eq '[' && defined($current_question)) {
             if ($row =~ m/^(\[\S\])/ ) {
-                $row =~ s/^(\[\S\]) //;
+                $row =~ s/^(\[\S\]) //; #remove brackets before option
                 $current_options{$row} = 1;
             }
             else {
@@ -106,19 +117,20 @@ sub get_questions_with_options($filename) {
     return %questions;
 }
 
-sub sanitize_questions($current_student_filename, %student_questions){
+sub sanitize_student_data($current_student_filename, %student_questions){
 
     say "... checking $current_student_filename";
+    # go through all master question and check if they are present in students file
     for my $current_master_question (keys %master_questions)
     {
         # missing question
         if(!defined($student_questions{$current_master_question})){
             say "missing question : " . $current_master_question;
-            #try to match and replace with another question
+            #try to match question and replace with another question
             my @student_questions = ( keys %student_questions );
             my $matching_question = lookup_similar_string($current_master_question,@student_questions);
 
-            if($matching_question){
+            if($matching_question){ # question matched
                 say "used this instead: $matching_question";
                 #replace matching question with master question
                 $student_questions{$current_master_question} = delete $student_questions{$matching_question};
@@ -132,10 +144,11 @@ sub sanitize_questions($current_student_filename, %student_questions){
 
             if (!defined($student_questions{$current_master_question}{$current_master_option})) {
                 say "missing answer   : $current_master_option";
-
+                #try to match option and replace with another option
                 my @student_options = ( keys %{$student_questions{$current_master_question} });
                 my $matching_option = lookup_similar_string($current_master_option,@student_options);
-                if($matching_option){
+
+                if($matching_option){ # option matched
                     say "used this instead: $matching_option";
                     #replace matching option with master option
                     $student_questions{$current_master_question}{$current_master_option}
@@ -149,21 +162,21 @@ sub sanitize_questions($current_student_filename, %student_questions){
 
 sub get_answers(%questions){
     my %answers;
-
+    # go through all questions and save the selected option in %answers
     for my $current_question (keys %questions) {
         $answers{$current_question} = undef;
-
+        # go through all options
         for my $current_option ( keys %{ $questions{$current_question} } ) {
-            #new answer
+            #option selected > save as answer
             if( $questions{$current_question}{$current_option}
                 && !defined($answers{$current_question})){
                 $answers{$current_question} = $current_option;
             }
-            #anwer already available
+            #more than one option selected > answer not valid
             elsif( $questions{$current_question}{$current_option}
                 && defined($answers{$current_question})){
                 $answers{$current_question} = undef;
-                last;
+                last; # duplicate selection, other options are ignored
             }
         }
     }
@@ -174,14 +187,17 @@ sub check_answers(%current_student_answers){
     my $answered = 0;
     my $answered_correct = 0;
 
+    # go through all master answers and compare with current student answers
     for my $current_question (keys %master_answers){
+        # student answer matches master answer
         if(defined($current_student_answers{$current_question})
                 &&     $master_answers{$current_question}
-                    eq $current_student_answers{$current_question}) { # correct answer
+                    eq $current_student_answers{$current_question}) {
             $answered++;
             $answered_correct++;
         }
-        elsif(defined($current_student_answers{$current_question})){ #wrong answer
+        # student answer mismatches master answer
+        elsif(defined($current_student_answers{$current_question})){
             $answered++;
         }
     }
@@ -192,12 +208,14 @@ sub lookup_similar_string($string_to_find, @library) {
 
     my $normalized_string_to_find = normalize_string($string_to_find);
 
+    # go through all string in array and find first similar
     for my $current_string (@library) {
         my $normalized_current_string = normalize_string($current_string);
 
+        #calculate Levenshtein distance
         my $distance = distance($normalized_current_string, $normalized_string_to_find);
 
-        #if edit_distance is less then 10% of string length
+        #if distance is less then 10% of string length > string similar
         if ($distance*10 < (length($normalized_string_to_find))) {
             return $current_string;
         }
@@ -206,6 +224,8 @@ sub lookup_similar_string($string_to_find, @library) {
 }
 
 sub normalize_string($string){
+
+    # stopwords to be removed from string
     my $stopwords = 'the|a|an|of|on|in|by|at|is|\'s|are|that|they|for|to|it';
 
     $string =~ s/\b(?:$stopwords)\b//g;  # remove stop words;
